c7d973697adee03f0d66d2677105850c1ba9d8fa
Vinicius Mocci <vinicius.mocci@elloxdigital.com>
2022-07-13 15:45:42 -0300

    #main: lesson 01 added

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [buttonTitle, setButtonTitle] = useState('Sou um botão! =D')
+
+  const changeButtonTitle = (user) => {
+
+    switch (user) {
+      case "Gugu":
+        setButtonTitle('Oiaaaaaaaaaaaaa')
+        break
+      
+      case "Faustao":
+        setButtonTitle('É essa fera, meu!')
+        break
+
+      case "Faro":
+        setButtonTitle('cavalo! Ui!')
+        break
+        
+      default:
+        setButtonTitle('O amigo, passa o parametro ae.')
+        break
+    }
+  }
+
+  return (
+    <div className="App">
+      <button onClick={() => changeButtonTitle('Gugu')}>{buttonTitle}</button>
+    </div>
+  );
+}
+
+export default App;
